028905c7b6373892db6afba6d1b817b076ac9b14
runningwinterberry <thegreateat1@gmail.com>
2022-06-17 10:02:57 -0600

    v1.2

---
+++ Genshin_Daily_Check-in.py
@@ -0,0 +1,94 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import webbrowser
+import platform
+import os
+import ctypes
+import requests
+
+version = 1.2
+
+command = 'cls'
+if os.name != 'nt':
+    command = 'clear'
+os.system(command)
+
+def newVersion():
+    response = requests.get("https://api.github.com/repos/runningwinterberry/Genshin-Daily-Check-in/releases/latest")
+    lv = float(response.json()["tag_name"].replace('v',''))
+    if version < lv:
+        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 5)
+        print("**********************************\nVersion v"+ str(lv) +" Available, Currently Running "+ str(version) +"\nWould you like to go to the releases page? \nhttps://github.com/runningwinterberry/Genshin-Daily-Check-in/releases \n**********************************")
+
+newVersion()
+    
+print("You \033[91m must \033[0m be logged in and have browser remember you \nso I can go directly to the daily check-in screen.\nAlso, please insure you browser opens in full screen.")
+
+print("\n\nPaste the url of the daily login screen and click enter")
+url = input()
+if url == "":
+    url = "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&mhy_auth_required=true&mhy_presentation_style=fullscreen&lang=en-us&bbs_theme=dark&bbs_theme_device=1"
+
+print("Please enter RGB value of orange seperated by commas without spaces \nthen click enter OR click enter to use defult (208,100,47)")
+a = input()
+if a == "":
+    a = "208,100,47"
+color = tuple(map(int, a.split(',')))
+
+print("Please enter your system of 'Mac', 'Windows', or 'Linux'\n(for this purpose there is no differnce between Windows and Linux) \nthen click enter OR click enter to use defult (Windows)")
+sys = input()
+if sys == "":
+    sys = "Windows"
+
+print("How long shall I wait (in seconds) till I collect your daily check-in? \nI will then collect it every 24 hours after")
+wait = input()
+if wait == "":
+    wait = "2"
+print("\033[1;32mSetup complete \033[0m")
+sleep(2)
+ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+sleep(int(wait)-2)
+
+def click(x, y):
+    pyautogui.moveTo(x, y)
+    sleep(0.1)
+    pyautogui.click()
+
+while 1:
+    webbrowser.open(url, new=1)
+    sleep(10)
+        
+    s = pyautogui.screenshot()
+    for x in range(s.width):
+        for y in range(s.height):
+            if s.getpixel((x, y)) == color:
+                ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+                click(x, y)
+                sleep(10)
+                newVersion()
+                if sys == "Mac":
+                    pyautogui.hotkey('command', 'w')
+                else:
+                    pyautogui.hotkey('ctrl', 'w')
+                break
+        else:
+            continue
+        break
+    else:
+        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 5)
+        os.system(command)
+        print("\033[91mColor not found!\033[0m")
+        newVersion()
+    sleep(86380)
+        
+
+
+
+
+
+
+
+
+        
